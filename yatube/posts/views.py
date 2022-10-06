c6d1466720b3f570622cdfb781f0b6292aa52a1c
Артём Малыш <rtmmlsh@yandex.ru>
2022-10-06 13:14:19 +0400

    Сделал правки во вью

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -140,7 +140,7 @@ def profile_follow(request, username):
 @login_required
 def profile_unfollow(request, username):
     author = get_object_or_404(User, username=username)
-    unfollow_author = Follow.objects.filter(
+    Follow.objects.filter(
         user=request.user, author=author
     ).delete()
 
